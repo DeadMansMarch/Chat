2dd1647c7bbbba7a3ca9026903d69bbf74c27de2
DeadMansMarch <lsp425@gmail.com>
2016-07-27 14:58:16 -0400

    Update Crypt.java

--- Crypt.java
+++ Crypt.java
@@ -7,18 +7,33 @@ package chat;
  */
 
 public class Crypt {
-    private int Key = 0;
     
-    public int GetKey(){
-        return Key;
+    public String pureMessage(String Text){
+        return Text.substring(3, Text.length() - 1);
     }
     
-    public void SetKey(int Key){
-       this.Key = Key; 
+    public String Encrypt(String Text,int Key){
+        String En = "";
+        for (Character K: Text.toCharArray()){
+            En = En + "-";
+            En = En + (((int) K) * Key);
+        }
+        System.out.println("Encrypted as :"  + En);
+        return En;
+    }
+    
+    public String Decrypt(String Encrypted,int Key){
+        String De = "";
+        for (String k: Encrypted.split("-")){
+            if (!k.equals("")){
+                De = De + (char) (Integer.parseInt(k) / Key);
+            }
+           
+        }
+        return De;
     }
     
     public Crypt(){
         
     }
 }
-
