e959b6229b48ae5ce19c4248ba5741f9623f3743
DeadMansMarch <lsp425@gmail.com>
2016-08-02 17:14:20 -0400

    Added basic naming.

--- Chat/src/chat/Connector.java
+++ Chat/src/chat/Connector.java
@@ -10,40 +10,115 @@ import java.net.Socket;
 
 /**
  *
- * @author Camper
+ * @author Liam Pierce  
  */
 public class Connector {
-    static TCPApi API = new TCPApi();
-    static Random RandCreator = new Random();
-    static Crypt EnDe = new Crypt();
-    static HashMap<String,Server> Servers = new HashMap<>();
+    public static TCPApi API;
+    public static Random RandCreator = new Random();
+    public static Crypt EnDe = new Crypt();
     
-    static HashMap<String,Integer> IpToConnection = new HashMap<>();
+    private static HashMap<String,Server> Servers = new HashMap<>();
+    private static HashMap<String,HashMap<String,Integer>> Connections = new HashMap<>();
+    private static HashMap<String,String> NameMap = new HashMap<>();
     
+    //Return map of all connections.
+    public static HashMap<String,HashMap<String,Integer>> getConnections(){
+        return Connections;
+    }
+    
+    //Get assigned name for IP.
+    public static String getName(String IP){
+        return NameMap.get(IP);
+    }
+    
+    public static String getIP(String Name){
+        String IP = "";
+        for (Map.Entry<String, String> NameSet : NameMap.entrySet()) {
+             if (NameSet.getValue().equals(Name)){
+                 IP = NameSet.getKey();
+                 break;
+             }
+        }
+        return IP;
+    }
+    
+    //Assign name for given IP.
+    public static void setName(String IP, String Name){
+        NameMap.put(IP,Name);
+    }
+    
+    public static void OneConnection(String MainIP,String IP){
+        Connections.get(MainIP).clear();
+        Connections.get(MainIP).put(IP,1);
+    }
+    
+    //Log an IP for sending.
+    public static void addConnection(String MainIP,String IP){
+        Connections.get(MainIP).put(IP,1);
+    }
+    
+    //Remove an IP for sending.
+    public static void removeConnection(String IP){
+        Connections.remove(IP);
+    }
+    
+    //Assign the map for sender as all logged IPs.
+    //TODO: Allow for new users to recieve chats by adding new users to default.
+    public static void createDefaultConnectionset(String MainIP){
+        Connections.put(MainIP,new HashMap<>());
+        Connections.forEach((String I,Object B) -> {
+            if (!I.equals(MainIP)){
+                addConnection(MainIP,I);
+            }
+        });
+    }
+    
+    //Send chat to specific user.
+    public static void sendAt(String IP,String Text){
+        Servers.get(IP).enSend(IP, Text);
+    }
+    
+    //Send chat to all users logged in server.
+    public static void sendAll(String CurrentIP,String Text){
+        Connections.forEach((String IP,Object B) -> {
+            if (!IP.equals(CurrentIP)){
+                Servers.get(IP).enSend(IP,Text);
+            }
+        });
+    }
+    
+    public static void sendConnectionset(String IP,String Text){
+        Connections.get(IP).forEach((String IPc,Integer B) -> {
+            Servers.get(IPc).enSend(IPc,Text);
+        });
+        
+    }
+    
+    //Constructor.
     public Connector(){
-        Server.IsServer = true;
         
     }
     
     public static void main(String[] args) {
-        System.out.println("Initiating...");
+        Server.IsServer = true;
+        Connector.API = new TCPApi();
         Connector C = new Connector();
-        
-        System.out.println("Initiated server...");
-        
+
+        //Thread for accepting new clients constantly.
         Thread ConnectionThread = new Thread(new Runnable(){
             @Override
             public void run(){
                 while (true){
-                    System.out.println("Waiting for connection...");
+                    API.Log("Waiting for new connection :");
                     Socket New = API.GetServerSocket(6789);
                     String IP = New.getInetAddress().toString().substring(1);
-                    System.out.println(IP);
+                    API.Log("Connection inititated for IP: " + IP);
                     Servers.put(IP,new Server(New));
                 }
             }
         },"Connector");
         
+        //Starts connector thread.
         ConnectionThread.start();
     }
 }
--- Chat/src/chat/TCPApi.java
+++ Chat/src/chat/TCPApi.java
@@ -12,23 +12,65 @@ import java.net.*;
  * @author Liam Pierce
  */
 public class TCPApi {
-    boolean Log = true;
+    private ServerSocket Accept;
+    public static boolean Log = true;
 
     final private HashMap<String,Socket> Connections = new HashMap<>();
-    final private HashMap<String,BufferedReader> Readers = new HashMap<>();
     final private HashMap<String,Timer> Timers = new HashMap<>();
     final private HashMap<String,HashMap<String,FuncStore>> ListenerActions = new HashMap<>();
     
+    //Logs a header.
+    private void logHeader(){
+        System.out.print("Log: ");
+    }
+
+    //Logs a string.
+    public void Log(String Text){
+        if (Log){
+            logHeader();
+            System.out.println(Text);
+        }
+    }
     
-    public void Connection(IP Host,String Name){
+    //Logs an object.
+    public void Log(Object Text){
+        if (Log){
+            logHeader();
+            System.out.println(Text);
+        }
+    }
+    
+    //Logs a boolean.
+    public void Log(boolean Text){
+        if (Log){
+            logHeader();
+            System.out.println(Text);
+        }
+    }
+    
+    //Logs a character.
+    public void Log(char Text){
+        if (Log){
+            logHeader();
+            System.out.println(Text);
+        }
+    }
+    
+    public boolean Connection(IP Host,String Name){
         try{
             Connections.put(Name,new Socket(Host.Converter(),Host.GetPort()));
+            if (Log == true){
+                System.out.println(" Connection Init Successful");
+            }
         }catch(Exception E){
             if (Log == true){
-                System.out.println("Connection to server: " + Host.GetIP() +
+                System.out.print(" Connection to server: " + Host.GetIP() +
                         " failed.");
+                return false;
             }
         }
+        
+        return true;
     }
     
     public void Send(String Connector,String Send){
@@ -37,58 +79,122 @@ public class TCPApi {
         try{
             Output = new DataOutputStream(To.getOutputStream());
             Output.writeBytes("::" + Send + "\n");
+            Output.flush();
+            Log("Sending Completed :" + Send);
         }catch(IOException E){
-            if (Log == true){
-                System.out.println("Message sending failed.");
-            }
+            Log("Sending failed.");
         }
     }
     
+    public Socket GetServerSocket(int Port){
+        Socket Try = null;
+        if (Accept == null){
+            try{
+                Accept = new ServerSocket(Port);
+            }catch(IOException E){
+                
+            }
+        }
+        try{
+            Try = Accept.accept();
+        }catch(Exception E){
+            Log("Server failed : " + E);
+        }
+        return Try;
+    }
+    
     public void CreateListener(int Port, String Name, FuncStore Action){
-        Socket Listener;
+        Socket Listener = null;
         InputStreamReader R = null;
         try{
-            Listener = new ServerSocket(Port).accept();
-            if (Log == true){
-                System.out.println("ServerSocket created successfully.");
-            }
+            Log("Working on listener.");
+            Listener = GetServerSocket(Port);
+            Log("ServerSocket created successfully.");
             R = new InputStreamReader(Listener.getInputStream());
         }catch(IOException E){
-            if (Log == true){
-                System.out.println("Socket creation failed: " + E);
-            }
+            Log("Socket creation failed with exception: " + E);
         }
+        
+        if (!ListenerActions.containsKey(Name)){
+            ListenerActions.put(Name, new HashMap<>());
+        }
+        
+        ListenerActions.get(Name).put("Main_Listener",Action);
         
         BufferedReader B = new BufferedReader(R);
-        Readers.put(Name,B);
         
-        if (!Action.equals(null)){
-            CreateListenerAction(Name,"Main_Listener",Action);
-        }
+        
         Timer Reader = new Timer();
         Timers.put(Name,Reader);
         try{
-            Reader.scheduleAtFixedRate(new TimerTsk(ListenerActions.get(Name),B.readLine()),10,300);
+            Reader.scheduleAtFixedRate(new TimerTsk(ListenerActions.get(Name),B,Listener),10,300);
+        }catch(Exception E){
+            Log("Error reading line.");
+        }
+    }
+    
+    public void CreateServerListener(Socket Listener, String Name, FuncStore Action){
+        InputStreamReader R = null;
+        try{
+            R = new InputStreamReader(Listener.getInputStream());
         }catch(IOException E){
-            if (Log == true){
-                System.out.println("Error reading line.");
-            }
+            Log("Stream reader failed with exception : " + E);
+        }
+        
+        if (!ListenerActions.containsKey(Name)){
+            ListenerActions.put(Name, new HashMap<>());
+        }
+        
+        ListenerActions.get(Name).put("Main_Listener",Action);
+        
+        BufferedReader B = new BufferedReader(R);
+
+        Timer Reader = new Timer();
+        Timers.put(Name,Reader);
+        
+        try{
+            Reader.scheduleAtFixedRate(new TimerTsk(ListenerActions.get(Name),B,Listener),10,300);
+        }catch(Exception E){
+            Log("Stream reader failed with exception : " + E);
+        }
+    }
+    
+    public void CloseConnection(String Name){
+        try{
+            Connections.get(Name).close();
+            RemoveListener(Name);
+            Connector.removeConnection(Name);
+        }catch(IOException E){
+            
         }
     }
     
     public void CreateListenerAction(String ListenerKey,String Name, FuncStore Action){
+        HashMap<String,FuncStore> Save = ListenerActions.get(ListenerKey);
+        if (Save == null){
+            ListenerActions.put(ListenerKey,new HashMap<>());
+        }
         ListenerActions.get(ListenerKey).put(Name,Action);
+    }
+    
+    public void CreateNilActionSet(String ListenerKey,String Name){
+        ListenerActions.put(ListenerKey,new HashMap<>());
     }
     
     public void RemoveListener(String Key){
         Timers.remove(Key);
-        Readers.remove(Key);
+        ListenerActions.remove(Key);
     }
     
-    public void RemoveListenerAction(String Listener,String Action){
-        ListenerActions.get(Listener).remove(Action);
+    public void RemoveListenerAction(String ListenerKey,String Action){
+        ListenerActions.get(ListenerKey).remove(Action);
     }
     
     public TCPApi(){
+        if (Server.IsServer){
+            Log("Creating server API.");
+        }else{
+            Log("Creating client API.");
+        }
     }
 }
