2606b6f392c1ca0dd58342116a8555d07b262fb9
DeadMansMarch <lsp425@gmail.com>
2016-08-04 21:26:48 -0400

    Final V1.1.1

--- Chat/src/chat/Client.java
+++ Chat/src/chat/Client.java
@@ -1,6 +1,3 @@
-
-/* @Error: 1 - HostNotFoundException;
-*/
 package chat;
 
 /**
@@ -8,56 +5,144 @@ package chat;
  * @author Liam Pierce
  */
 public class Client{
-    static String Last;
-    TCPApi API = new TCPApi();
-    Crypt EnDe;
-    String User = "Guest";
-    String Pass = null;
-    int Key = 1;
+    private String Serverpass;
+    static TCPApi API = new TCPApi(false);
+    static Crypt EnDe;
+    static int Key = 0;
+    static UI MainUI;
+    public static ServerLogin NewLogin;
+    public static Thread RunUI;
     
-    private boolean ProtocolC(){
-        API.Connection(new IP("136.167.171.151",6789),"Main");
-        API.CreateListener(6789,"", null);
+    //Disconnects MainUI
+    public static void Disconnect(){
+        MainUI.dispose();
+    }
+    
+    private IP Server;
+    
+    //Main connection protocol.
+    public boolean protocolC(String Password){
+        this.Serverpass = Password;
+        API.connection(Server,"Main");
         
-        API.CreateListenerAction("Main","ProtocolC",
-                new FuncStore("MainConnectionProtocol"){
+        API.send("Main","Connect?");
+        
+        API.createListener(6789,"Main", new FuncStore("MainConnectionProtocol"){
             @Override
             void Run(String Text){
-                Client.Last = Text;
-                ConnectionProtocolAssist(Text);
+                connectionProtocolAssist(Text);
             }
         });
         return false;
     }
     
-    private void EnSend(String Socket,String Message){
-        API.Send(Socket,EnDe.Encrypt(Message,Key));
-    }
-    
-    private void ConnectionProtocolAssist(String K){
-        Client.Last = K;
-        
+    //Intercept for connection protocol.
+    private void connectionProtocolAssist(String K){
+        System.out.println(K);
         switch(K){
-            
-            case "::OK\n":
-                API.Send("Main","Encrypt?");
+            case "::OK":
+                RunUI.start();
+                API.send("Main","Encrypt?");
                 break;
-            
-            case "::Connected\n":
-                EnSend("Main", "Handled.");
+            case "::Connected":
+                
+                API.send("Main", K);
                 break;
-            case "Information?":
-                EnSend("Main",User);
             default:
-                Key = Integer.parseInt(K.substring(3, K.length() - 1));
+                String[] DeMessage = EnDe.Decrypt(K.substring(2), Key).split(":");
+                if (DeMessage[0].equals("GoodPass")){
+                    enSend("Main","@Start");
+                }else if (DeMessage[1].equals("BadPass")){
+                    NewLogin.dispose();
+                }else if (Key != 0 && EnDe.Decrypt(K.substring(2), Key).equals("SessionStart")){
+                    
+                    API.removeListenerAction("Main","Main_Listener");
+                    API.createListenerAction("Main", "Communication", new FuncStore("Connection"){
+                        @Override
+                        public void Run(String Text){
+                            String DeMessage = EnDe.Decrypt(Text.substring(2), Key);
+                            String ID[] = DeMessage.split(":");
+                            
+                            if (ID[0].equals("IsName")){
+                                System.out.println("??");
+                                MainUI.changeName(ID[1],ID[2]);
+                            }else if (ID[0].equals("ConnectionSet")){
+                                System.out.println("Set");
+                                MainUI.Update(ID);
+                            }else if (ID[0].equals("AddName")){
+                                //MainUI.ipList(ID[1]);
+                            }else if(ID[0].equals("Rem")){
+                                 MainUI.nameRemove(ID[1]);   
+                            }else if(ID[0].substring(0,3).equals("|||")){
+                                MainUI.addMessage(ID[0].substring(3),ID[1]);
+                            }
+                        }
+                    });
+                    
+                }else{
+                    Key = Integer.parseInt(K.substring(2));
+                    enSend("Main","Password:" + Serverpass);
+                }
                 break;
         }
     }
     
+    //Sets the connectionset to NameOrIP.
+    public static void ChangeConnectionset(String NameOrIP){
+        enSend("Main","Connectionset:" + NameOrIP);
+    }
+    
+    //Sets the clients connectionset to default.
+    public static void DefaultConnectionset(){
+        enSend("Main","DefaultConnectionset");
+    }
+    
+    //Sending encrypted messages.
+    public static void enSend(String Connection,String Text){
+        API.send(Connection,EnDe.Encrypt(Text, Key));
+    }
+    
+    //Main method.
+    
     public static void main(String[] args) {
-        Client Chat = new Client();
-        Chat.EnDe = new Crypt();
-        String K = Chat.EnDe.Decrypt(Chat.EnDe.Encrypt("StraightUpCancer", 200), 200);
-        System.out.println("@Done" + K + "-");
+        API.log("Attempting connection to server...");
+        
+        //UI to be run after login.
+        RunUI = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                MainUI = new UI();
+            }
+        },"UI Cancer");
+        
+        //UI for login to be run right away.
+        Thread W = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                NewLogin = new ServerLogin();
+            }
+        },"UI Tumor");
+        W.start();
+        
+        //This some trash.
+        while (NewLogin == null){
+            try{
+                Thread.sleep(10); // And this is the line that fixed an hour of bug testing.
+            }catch(Exception E){
+                
+            }
+        }
+        NewLogin.login();
+        
+        String IP = NewLogin.getIP();
+        String Password = NewLogin.getPassword();
+        Client NewClient = new Client(IP,Password);
+        Client.EnDe = new Crypt();
+    }
+    
+    
+    public Client(String IP,String Password){
+         Server = new IP(IP,6789);
+         protocolC(Password);
     }
 }
--- Chat/src/chat/Crypt.java
+++ Chat/src/chat/Crypt.java
@@ -1,39 +1,86 @@
-
 package chat;
-
+ 
+import java.nio.charset.StandardCharsets;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+ 
 /**
- *
  * @author Liam Pierce
+ * @author Gregory Conrad
  */
-
+ 
+/*
+ * Original Author: Liam Pierce
+ * Improved By: Gregory Conrad
+ */
+ 
 public class Crypt {
+    static String IV = "AAAAAAAAAAAAAAAA";
     
-    public String pureMessage(String Text){
-        return Text.substring(3, Text.length() - 1);
-    }
-    
-    public String Encrypt(String Text,int Key){
+    //Encode messages using AES
+    private String Encryptor(String Text,int Key) throws Exception{
+        //Original Code:
+        /*
         String En = "";
         for (Character K: Text.toCharArray()){
             En = En + "-";
-            En = En + (((int) K) * Key);
+            En = En + (((int) K + 1) * Key);
         }
-        System.out.println("Encrypted as :"  + En);
         return En;
+        */
+       
+        //New Code:
+        String newKey = Integer.toString(Key);
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+        SecretKeySpec key = new SecretKeySpec(newKey.getBytes("UTF-8"), "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+        byte[] encrypted = cipher.doFinal(Text.getBytes("UTF-8"));
+        return new String(encrypted, "UTF-8");
     }
-    
-    public String Decrypt(String Encrypted,int Key){
+   
+    public String Encrypt(String Text, int Key){
+        try{
+            return Encryptor(Text,Key);
+        }catch(Exception E){
+            return E.toString();
+        }
+    }
+   
+    //Decrypts using AES
+    private String Decryptor(String Encrypted,int Key) throws Exception{
+        //Original Code:
+        /*
         String De = "";
         for (String k: Encrypted.split("-")){
             if (!k.equals("")){
-                De = De + (char) (Integer.parseInt(k) / Key);
+                De = De + (char) ((Integer.parseInt(k) - 1) / Key);
             }
            
         }
-        return De;
+        */
+       
+        //New Code:
+        String newKey = Integer.toString(Key);
+        byte[] EncryptedBytes = Encrypted.getBytes(StandardCharsets.UTF_8);
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+        SecretKeySpec key = new SecretKeySpec(newKey.getBytes("UTF-8"), "AES");
+        cipher.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+        return new String(cipher.doFinal(EncryptedBytes),"UTF-8");
+       
     }
     
+    
+    public String Decrypt(String Text, int Key){
+        try{
+            return Decryptor(Text,Key);
+        }catch(Exception E){
+            return E.toString();
+        }
+    }
+    
+    
     public Crypt(){
-        
+       
     }
 }
